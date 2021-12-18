5978e3e1f37fa0de5d3395d8334b1341b60c9a74
Antônio Carlos <juniorbecari@gmail.com>
2021-12-18 09:49:01 -0300

    Agora o botão Conferir detecta se não tem nada na caixa Dezenas e emite um alerta para o usuário

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const loteriaBox = document.querySelector("select[name='loteria']");
 const mesSorteBox = document.querySelector(".mes-sorte");
 const timeCoracaoBox = document.querySelector(".time-coracao");;
 
+const dezenasBox = document.querySelector(".input-large textarea");
+const conferirBtn = document.querySelector(".input-large button");
+
 document.onclick = updateBoxes;
 window.onload = updateBoxes;
+
+conferirBtn.onclick = check;
 
 function updateBoxes() {
     if (loteriaBox.value !== "dia-de-sorte")
@@ -17,3 +22,8 @@ function updateBoxes() {
     else
         timeCoracaoBox.classList.remove("disabled");
 };
+
+function check() {
+    if (dezenasBox.value === "")
+        alert("Você deve colocar algumas dezenas apostadas na caixa Dezenas!");
+}
