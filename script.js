97b4415f10314d8927419cc33ae73a231177dcfd
Antônio Carlos <juniorbecari@gmail.com>
2021-12-20 10:00:42 -0300

    Agora ele já faz request pra APi

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const loteriaBox = document.querySelector("select[name='loteria']");
+const concursoBox = document.getElementById("concurso-box");
 
 const mesSorteBox = document.querySelector(".mes-sorte");
 const timeCoracaoBox = document.querySelector(".time-coracao");;
 
 const dezenasBox = document.querySelector(".input-large textarea");
 const conferirBtn = document.querySelector(".input-large button");
+
+const apiUrl = "https://loteriascaixa-api.herokuapp.com/api";
 
 document.onclick = updateBoxes;
 window.onload = updateBoxes;
@@ -23,7 +26,23 @@ function updateBoxes() {
         timeCoracaoBox.classList.remove("disabled");
 };
 
-function check() {
+function check() { // se a pessoa colocar na ordem errada ordenar da forma certa
     if (dezenasBox.value === "")
         alert("Você deve colocar algumas dezenas apostadas na caixa Dezenas!");
+    else {
+        var loteria = loteriaBox.value;
+        var concurso = concursoBox.value === "" ? "latest" : concursoBox.value;
+        
+        var json = getJSON(apiUrl + "/" + loteria + "/" + concurso);
+    }
+}
+
+// eu sei que o xmlhttprequest está deprecated...
+function getJSON(url) {
+    var http = new XMLHttpRequest();
+    
+    http.open("GET", url, false);
+    http.send(null);
+    
+    return JSON.parse(http.responseText);
 }
